67cb9084369ae07454081f55485eae15e1931eaa
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-16 14:00:23 +0200

    add: snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,3 +3,8 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
+//console.log("Funzioni???");
+
+names.forEach(string => {
+    console.log(string);
+});
